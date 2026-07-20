d14d7b8e7a5b06c89f37281daafb06ed3693eabc
Devashish Jadhav <jadhavom24@gmail.com>
2026-07-20 13:18:33 +0530

    chore: remove unused import left over from CacheErrorHandler fix

--- backend/src/main/java/com/skillswap/config/CacheConfig.java
+++ backend/src/main/java/com/skillswap/config/CacheConfig.java
@@ -6,7 +6,6 @@ import org.springframework.cache.Cache;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.CacheErrorHandler;
-import org.springframework.cache.interceptor.SimpleCacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
